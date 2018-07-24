270f494cd0338785b94f3a2dfa240c12c7a6dea3
Mekolaos <31978152+Mekolaos@users.noreply.github.com>
2018-07-24 18:48:41 +0000

    Add files via upload
    First actual commit

---
+++ AntAut.py
@@ -0,0 +1,33 @@
+lettres ='abcdefghijklmnopqrstuvwxyz'
+
+def crypting(n, plaintext):
+	letter=''
+
+	for l in plaintext:
+		try:	
+			i =(letters.index(l) + int(n)) % 26
+			letter += letters[i]
+		except ValueError:
+			letter += l
+	return letter
+
+
+def decrypting(n, ciphertext):
+	letter = ''
+
+	for l in ciphertext:
+		try:
+			i = (letters.index(l) - int(n)) % 26
+			letter += letters[i]
+		except ValueError:
+			letter += l
+	return letter
+
+tradText = input("Your text : ")
+offset = input("Offset : ")
+
+crypter = crypting(offset, tradText)
+print('Crypted message : ', crypter)
+
+decrypter = decrypting(decalage, crypter)
+print('Decrypted message: ', decrypter)
